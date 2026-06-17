ec4088a3db2c64fe21afb020e438ff55a0e18fc9
Deena Randolph <deena.randolph@bellevuecollege.edu>
2026-06-17 06:03:18 -0700

    Updated Home Screen with new button and formatting.

--- app/src/main/java/com/example/pokesense/ui/HomeScreen.kt
+++ app/src/main/java/com/example/pokesense/ui/HomeScreen.kt
@@ -1,10 +1,17 @@
 package com.example.pokesense.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 // HomeScreen = first screen user sees
 @Composable
@@ -13,14 +20,34 @@ fun HomeScreen(
     onStartEncounter: () -> Unit
 ) {
     Column(
-        modifier = modifier
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("PokeSense")
+        Text(
+            text = "PokeSense",
+            Modifier.padding(20.dp),
+            fontSize = 44.sp
+        )
 
         Button(
-            onClick = onStartEncounter
+            onClick = onStartEncounter,
+            Modifier.padding(10.dp)
+
         ) {
-            Text("Initiate Encounter")
+            Text(
+                text = "Initiate Encounter",
+                fontSize = 25.sp
+            )
+        }
+        Button(
+            onClick = onStartEncounter, //loadCaughtPokemon()
+            Modifier.padding(10.dp)
+        ) {
+            Text(
+                text = "Caught Pokemon",
+                fontSize = 25.sp
+            )
         }
     }
 }
